0a3ac1aaa8e5c7d99607b85e1d616baf0807b60a
ijustwantabooklog <hjkspare@gmail.com>
2026-04-17 16:08:08 -0400

    Update firebase.js

--- booklog 2/src/firebase.js
+++ booklog 2/src/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
+const app = initializeApp({
   apiKey: "AIzaSyBbivACNBewoy5fa7YaqNHRn3VOis49wCY",
   authDomain: "booklog-b84d0.firebaseapp.com",
   projectId: "booklog-b84d0",
   storageBucket: "booklog-b84d0.firebasestorage.app",
   messagingSenderId: "50866962623",
-  appId: "1:50866962623:web:a262a1adbd63257382c60a",
-};
+  appId: "1:50866962623:web:a262a1adbd63257382c60a"
+});
 
-const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
